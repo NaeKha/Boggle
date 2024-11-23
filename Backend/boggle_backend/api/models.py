8f24c2e18dc21b27bd7b92d8d16836845b7cf04b
ZerekH <zerekhenderson@gmail.com>
2024-11-23 16:06:39 -0500

    assignment 5 up to views

--- Backend/boggle_backend/api/models.py
+++ Backend/boggle_backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Games(models.Model):
+    name = models.CharField(max_length=25)
+    size = models.IntegerField()
+    grid = models.TextField() # Serialize the 2d array to a string:
+    foundwords = models.TextField() #Serialize the array of words to a single string:
+
+    def __str__(self):
+        return f'Name: {self.name} Size: {self.size} Grid: {self.grid}'
